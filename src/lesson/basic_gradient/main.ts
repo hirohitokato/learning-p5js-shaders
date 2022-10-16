a77c1eef897552505539e7d4595213558111e46f
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2022-10-17 06:22:29 +0900

    Implement basic_gradient.
    https://itp-xstory.github.io/p5js-shaders/#/./docs/examples/basic_gradient

---
+++ src/lesson/basic_gradient/main.ts
@@ -0,0 +1,37 @@
+import type p5 from "p5";
+
+export let shaders: p5.Shader;
+
+export const preload = (p: p5): void => {
+  shaders = p.loadShader(
+    "shaders/basic_gradient/shader.vert",
+    "shaders/basic_gradient/shader.frag"
+  );
+};
+
+export const setup = (p: p5): void => {
+  // disables scaling for retina screens which can create inconsistent scaling between displays
+  p.pixelDensity(1);
+
+  // shaders require WEBGL mode to work
+  p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
+  p.noStroke();
+};
+
+export const windowResized = (p: p5, event?: Event): void => {
+  p.resizeCanvas(p.windowWidth, p.windowHeight);
+};
+
+export const draw = (p: p5): void => {
+  p.background(220);
+  // シェーダーに値を送れる
+  shaders.setUniform("u_resolution", [p.width, p.height]);
+
+  // // we divide millis by 1000 to convert it to seconds
+  // shaders.setUniform("u_time", p.millis() / 1000.0);
+  // // we flip Y so it's oriented properly in our shader
+  // shaders.setUniform("u_mouse", [p.mouseX, p.map(p.mouseY, 0, p.height, p.height, 0)]);
+
+  p.shader(shaders);
+  p.rect(0, 0, p.width / 2, p.height / 2);
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import p5 from "p5";
 import { createSketch } from "./p5-util/sketch";
-import { preload, setup, windowResized } from "./setup";
-import { draw } from "./draw";
+import { preload, setup, windowResized, draw } from "./lesson/basic_gradient/main";
 
 const sketch = createSketch({
   preload,
