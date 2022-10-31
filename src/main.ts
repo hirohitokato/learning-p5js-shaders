be1867050992ee5f60183619383d1e06804d0670
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2022-11-01 00:10:34 +0900

    Reroute main.ts but not worked well

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import p5 from "p5";
 import { createSketch } from "./p5-util/sketch";
-import { preload, setup, windowResized, draw } from "./lesson/shaders_to_shapes/program";
+import { preload, setup, windowResized, draw } from "./lesson/shaders_to_vertices/program";
 
 const sketch = createSketch({
   preload,
